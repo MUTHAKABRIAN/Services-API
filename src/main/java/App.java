1e8294a676c87a623973545799776736868fb36b
ShemBrooklyne <shemnyatti@gmail.com>
2021-04-14 09:15:16 +0300

    updated get and post method

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,7 @@ import Dao.*;
 import Models.Categories;
 import Models.Cleaner;
 import Models.Maid;
+import Models.MovingHelp;
 import com.google.gson.Gson;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
@@ -14,6 +15,7 @@ import java.sql.SQLException;
 
 import static spark.Spark.port;
 import static spark.Spark.post;
+import static spark.Spark.get;
 import static spark.route.HttpMethod.get;
 
 public class App {
@@ -21,6 +23,7 @@ public class App {
         Sql2oCategoriesDao categoriesDao;
         Sql2oCleanerDao cleanerDao;
         Sql2oMaidDao maidDao;
+        Sql2oMovingDao movingDao;
         Connection conn;
         Gson gson = new Gson();
 
@@ -33,9 +36,10 @@ public class App {
         categoriesDao = new Sql2oCategoriesDao(sql2o);
         cleanerDao = new Sql2oCleanerDao(sql2o);
         maidDao = new Sql2oMaidDao(sql2o);
+        movingDao = new Sql2oMovingDao(sql2o);
         conn = sql2o.open();
 
-        port(7070);
+        port(6060);
 
 
         //New data entry in cleaners table in DB
@@ -54,16 +58,30 @@ public class App {
             return gson.toJson(maid);
         });
 
+        post("moving_help/new", "application/json", (request, response) -> {
+            MovingHelp movingHelp = gson.fromJson(request.body(), MovingHelp.class);
+            movingDao.add(movingHelp);
+            response.status(201);
+            return gson.toJson(movingHelp);
+        });
+
         //Request for access of data in tables
-//        get("/cleaner", "application/json", ((request, response) -> {
-//            System.out.println(cleanerDao.getAll());
-//            return gson.toJson(cleanerDao.getAll());
-//        }));
-//
-//        get("/maid", "application/json", ((request, response) -> {
-//            System.out.println(maidDao.getAll());
-//            return gson.toJson(maidDao.getAll());
-//        }));
+        get("/cleaner", "application/json", ((request, response) -> {
+            System.out.println(cleanerDao.getAll());
+            return gson.toJson(cleanerDao.getAll());
+        }));
+
+        get("/maid", "application/json", ((request, response) -> {
+            System.out.println(maidDao.getAll());
+            return gson.toJson(maidDao.getAll());
+        }));
+
+        get("/moving_help", "application/json", ((request, response) -> {
+            System.out.println(movingDao.getAll());
+            return gson.toJson(movingDao.getAll());
+        }));
+
+        //get specific cleaner by name.
     }
 
 
